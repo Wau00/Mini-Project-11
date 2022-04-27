e902e377be82b251fd4e48972e10b7b9a854bfd4
walterunderwood <walonsounderwood@gmail.com>
2022-04-27 12:32:48 -0700

    Push 5

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,28 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)))
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  console.log(req.body);
+  const { errors } = req.body;
+
+  if(req.body) {
+    const newDiagnostic = { 
+      error_id:uuidv4(),
+      errors,
+          tip,
+          topic,
+          username,
+    };
+    readAndAppend(newDiagnostic, './db/diagnostics.json');
+    res.json(`Diagnostic has been posted successfully 🚀`);
+  }else{
+    res.error('Error in adding diagnostic');
+  }
 });
 
 module.exports = diagnostics;
